b74225d999b5d55dce834da30ef255fb6be98fb0
Michael Ellis <michael94ellis@gmail.com>
2020-11-25 09:54:53 -0500

    Initial Commit

---
+++ IMVDb/IMVDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMVDbApp.swift
+//  IMVDb
+//
+//  Created by Michael Ellis on 11/25/20.
+//
+
+import SwiftUI
+
+@main
+struct IMVDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
